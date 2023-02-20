7f47a096e9bd3290797b25aae6d7bbdddb06ec21
eokrizz <crisvagnersd@gmail.com>
2023-02-20 01:34:58 -0300

    ajustando a serialização de posts/notes

--- controllers/Notes.py
+++ controllers/Notes.py
@@ -43,10 +43,14 @@ def get_post(current_user, note_id):
         note = Notes.query.filter_by(
             user_id=user['id'], id=note_id).first()
 
-        print(note)
         note_dict = note_schema.dump(note)
 
-        return jsonify({'Note': note_dict}), 200
+        if len(note_dict) > 0:
+            note_dict['author'] = user['email']
+
+            return jsonify({'Note': note_dict}), 200
+
+        return jsonify({'message': 'Note Not Found'}), 404
 
     except:
         return jsonify({'message': 'Note Not Found'}), 404
@@ -59,14 +63,19 @@ def get_all_posts(current_user):
         user = user_schema.dump(current_user)
 
         notes = Notes.query.filter_by(user_id=user['id']).all()
-        print(notes)
-        notes_dict = notes_schema.dump(notes)
-        print(notes_dict)
 
-        return jsonify({'Notes': notes_dict}), 200
+        notes_dict = notes_schema.dump(notes)
+        for object in notes_dict:
+            if len(object) > 0:
+                object['author'] = user['email']
+
+        if len(notes_dict) > 0:
+            return jsonify({'Notes': notes_dict}), 200
+
+        return jsonify({'message': 'Not Found'}), 404
 
     except:
-        return jsonify({'message': 'Not Found'}), 404
+        return jsonify({'error': 'Not Found'}), 404
 
 
 @app.route('/notes/<int:note_id>', methods=['PUT'])
@@ -75,7 +84,7 @@ def update_post(current_user, note_id):
     try:
         user = user_schema.dump(current_user)
 
-        note = Notes.query.filter_by(user_id=user['id']).get(note_id)
+        note = Notes.query.filter_by(user_id=user['id'], id=note_id).first()
 
         title = request.json['title']
         content = request.json['content']
