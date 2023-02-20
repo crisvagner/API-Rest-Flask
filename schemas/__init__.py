e459e4349dab1f67b462200a8830c5abcd5c5178
eokrizz <crisvagnersd@gmail.com>
2023-02-19 21:35:39 -0300

    alterando diretório dos schemas

---
+++ schemas/__init__.py
@@ -0,0 +1,13 @@
+from server import ma
+
+
+# ESQUEMA DE USUÁRIOS USANDO ORM Marshmallow
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'email')
+
+
+# ESQUEMA DE TAREFAS USANDO ORM Marshmallow
+class NoteSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'content', 'author')
